2833278a1c379f891a1b24f8c12bd124c04be4f2
Poornima <poornimakharvi805@gmail.com>
2023-01-04 16:47:14 +0530

    Code Chages

---
+++ CalciResultClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalci
+{
+    public class CalciResultClass
+    {
+        public string firstnumber { get; set; }
+        public string secondnumber { get; set; }
+        public string result { get; set; }
+        public string operation { get; set; }
+        public string CreatedOn { get; set; }
+    }
+}
